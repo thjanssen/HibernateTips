edae2abd512a18a0eecab2b79e33c3eb01dbce00
Thorben <j2ee@streber24.de>
2016-05-27 18:07:36 +0200

    Add recipes to log SQL statements

---
+++ LogSQLStatements/src/test/java/org/thoughts/on/java/model/TestLogging.java
@@ -0,0 +1,41 @@
+package org.thoughts.on.java.model;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestLogging {
+
+	Logger log = Logger.getLogger(this.getClass().getName());
+
+	private EntityManagerFactory emf;
+
+	@Before
+	public void init() {
+		emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+	}
+
+	@After
+	public void close() {
+		emf.close();
+	}
+
+	@Test
+	public void selectAuthors() {
+		log.info("... selectAuthors ...");
+
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		em.createQuery("SELECT a FROM Author a WHERE a.id = 1", Author.class)
+				.getSingleResult();
+
+		em.getTransaction().commit();
+		em.close();
+	}
+}
