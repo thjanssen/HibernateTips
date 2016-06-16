cc5ab2ce7879bf56e808dd87b3f38d0a3d64a4db
Thorben <j2ee@streber24.de>
2016-06-16 04:58:53 +0200

    Add code samples for "Hibernate Tips: How to map a view with Hibernate"

---
+++ DatabaseViews/src/test/java/org/thoughts/on/java/model/TestViewEntity.java
@@ -0,0 +1,61 @@
+package org.thoughts.on.java.model;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestViewEntity {
+
+	Logger log = Logger.getLogger(this.getClass().getName());
+
+	private EntityManagerFactory emf;
+
+	@Before
+	public void init() {
+		emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+	}
+
+	@After
+	public void close() {
+		emf.close();
+	}
+
+	@Test
+	public void selectFromView() {
+		log.info("... selectFromView ...");
+
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		List<BookView> bvs = em.createQuery("SELECT v FROM BookView v", BookView.class)
+				.getResultList();
+
+		for (BookView bv : bvs) {
+			log.info(bv.getTitle() + " was written by "+bv.getAuthors());
+		}
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	@Test
+	public void selectUpdateView() {
+		log.info("... selectUpdateView ...");
+
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		BookView bv = em.find(BookView.class, 1L);
+		bv.setTitle("updated");
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+}
