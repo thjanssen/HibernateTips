7500ced8f2c437b907157f3be023de46530c5e20
Thorben <j2ee@streber24.de>
2016-06-23 04:46:14 +0200

    JOIN FETCH statements

---
+++ JoinFetch/src/test/java/org/thoughts/on/java/model/TestJoinFetch.java
@@ -0,0 +1,60 @@
+package org.thoughts.on.java.model;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.log4j.Logger;
+import org.hibernate.LazyInitializationException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestJoinFetch {
+
+	Logger log = Logger.getLogger(this.getClass().getName());
+
+	private EntityManagerFactory emf;
+
+	@Before
+	public void init() {
+		emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+	}
+
+	@After
+	public void close() {
+		emf.close();
+	}
+
+	@Test(expected = LazyInitializationException.class)
+	public void selectFromWithoutJoinFetch() {
+		log.info("... selectFromWithoutJoinFetch ...");
+
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		Author a = em.createQuery("SELECT a FROM Author a WHERE id = 1", Author.class).getSingleResult();
+		
+		em.getTransaction().commit();
+		em.close();
+		
+		log.info(a.getFirstName()+" "+a.getLastName()+" wrote "+a.getBooks().size()+" books.");
+	}
+	
+	@Test
+	public void selectFromWithJoinFetch() {
+		log.info("... selectFromWithJoinFetch ...");
+
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		Author a = em.createQuery("SELECT a FROM Author a JOIN FETCH a.books WHERE a.id = 1", Author.class).getSingleResult();
+		
+		em.getTransaction().commit();
+		em.close();
+		
+		log.info(a.getFirstName()+" "+a.getLastName()+" wrote "+a.getBooks().size()+" books.");
+	}
+}
