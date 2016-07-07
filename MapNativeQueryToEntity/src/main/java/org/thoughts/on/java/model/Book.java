c3b73b81e5f5a2597ff9727b85b7653feb539384
Thorben <j2ee@streber24.de>
2016-07-07 04:27:50 +0200

    Use @SQLResultSetMappings to map the result of a native query to an entity.

---
+++ MapNativeQueryToEntity/src/main/java/org/thoughts/on/java/model/Book.java
@@ -0,0 +1,143 @@
+package org.thoughts.on.java.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityResult;
+import javax.persistence.FieldResult;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OrderBy;
+import javax.persistence.SqlResultSetMapping;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Version;
+
+@Entity
+@SqlResultSetMapping(
+		name = "BookMapping", 
+		entities = @EntityResult(
+						entityClass = Book.class, 
+						fields = {
+								@FieldResult(name = "id", column = "id"),
+								@FieldResult(name = "version", column = "version"),
+								@FieldResult(name = "title", column = "title"),
+								@FieldResult(name = "publishingDate", column = "publishingDate"),
+								@FieldResult(name = "publisher", column = "publisherid")}))
+public class Book implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+	@Version
+	@Column(name = "version")
+	private int version;
+
+	@Column
+	private String title;
+
+	@Column
+	@Temporal(TemporalType.DATE)
+	private Date publishingDate;
+
+	@ManyToOne
+	@JoinColumn(name = "publisherid")
+	private Publisher publisher;
+
+	@ManyToMany
+	@JoinTable(name = "BookAuthor", joinColumns = { @JoinColumn(name = "bookId", referencedColumnName = "id") }, inverseJoinColumns = { @JoinColumn(name = "authorId", referencedColumnName = "id") })
+	private Set<Author> authors = new HashSet<Author>();
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(final int version) {
+		this.version = version;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Book)) {
+			return false;
+		}
+		Book other = (Book) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getPublishingDate() {
+		return publishingDate;
+	}
+
+	public void setPublishingDate(Date publishingDate) {
+		this.publishingDate = publishingDate;
+	}
+
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (title != null && !title.trim().isEmpty())
+			result += "title: " + title;
+		return result;
+	}
+
+	public Publisher getPublisher() {
+		return this.publisher;
+	}
+
+	public void setPublisher(final Publisher publisher) {
+		this.publisher = publisher;
+	}
+
+	public Set<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(Set<Author> authors) {
+		this.authors = authors;
+	}
+}
---
+++ MapNativeQueryToEntity/src/test/java/org/thoughts/on/java/model/TestMapNativeQuery.java
@@ -0,0 +1,60 @@
+package org.thoughts.on.java.model;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestMapNativeQuery {
+
+	Logger log = Logger.getLogger(this.getClass().getName());
+
+	private EntityManagerFactory emf;
+
+	@Before
+	public void init() {
+		emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+	}
+
+	@After
+	public void close() {
+		emf.close();
+	}
+
+	@Test
+	public void implicitMapping() {
+		log.info("... implicitMapping ...");
+
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		Book b = (Book) em.createNativeQuery("SELECT * FROM book b WHERE id = 1", Book.class).getSingleResult();
+		Assert.assertTrue(b instanceof Book);
+		Assert.assertEquals(new Long(1), b.getId());
+		Assert.assertEquals("Effective Java", b.getTitle());
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	@Test
+	public void explicitMapping() {
+		log.info("... explicitMapping ...");
+
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		Book b = (Book) em.createNativeQuery("SELECT * FROM book b WHERE id = 1", "BookMapping").getSingleResult();
+		Assert.assertTrue(b instanceof Book);
+		Assert.assertEquals(new Long(1), b.getId());
+		Assert.assertEquals("Effective Java", b.getTitle());
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+}
