29bff805191abd2423a22821663c87b901d03ec1
Thorben Janssen <thorben@thoughts-on-java.org>
2017-02-02 10:55:07 +0100

    Add more examples

---
+++ MapEnumerations/src/main/java/org/thoughts/on/java/model/Author.java
@@ -0,0 +1,108 @@
+package org.thoughts.on.java.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+@Entity
+public class Author implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+
+	@Version
+	@Column(name = "version")
+	private int version;
+
+	@Column
+	private String firstName;
+
+	@Column
+	private String lastName;
+
+	@Enumerated(EnumType.STRING)
+	private AuthorStatus status;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(final int version) {
+		this.version = version;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Author)) {
+			return false;
+		}
+		Author other = (Author) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public AuthorStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(AuthorStatus status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", version=" + version + ", firstName="
+				+ firstName + ", lastName=" + lastName + ", status=" + status
+				+ "]";
+	}
+}
