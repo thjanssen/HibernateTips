76f9549c1903d32b89c9fc7e1d13acbf22213b5e
Thorben Janssen <thorben@thoughts-on-java.org>
2017-02-02 10:46:45 +0100

    Fix DB setup

--- MapGeneratedColumns/src/test/resources/create.sql
+++ MapGeneratedColumns/src/test/resources/create.sql
@@ -1,24 +1,6 @@
-CREATE TABLE author
-(
-  id bigint NOT NULL,
-  firstname character varying(255),
-  lastname character varying(255),
-  lastupdate timestamp without time zone,
-  version integer NOT NULL,
-  CONSTRAINT author_pkey PRIMARY KEY (id)
-);
+CREATE TABLE author(id bigint NOT NULL, firstname character varying(255), lastname character varying(255), lastupdate timestamp without time zone, version integer NOT NULL, CONSTRAINT author_pkey PRIMARY KEY (id));
+CREATE SEQUENCE hibernate_sequence INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1;
 
-CREATE OR REPLACE FUNCTION sync_lastupdate() RETURNS trigger AS $$
-BEGIN
-  NEW.lastupdate := NOW();
+CREATE OR REPLACE FUNCTION sync_lastupdate() RETURNS trigger AS $$ BEGIN NEW.lastupdate := NOW(); RETURN NEW; END; $$ LANGUAGE plpgsql;
 
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER
-  sync_lastupdate
-BEFORE INSERT OR UPDATE ON
-  author
-FOR EACH ROW EXECUTE PROCEDURE
-  sync_lastupdate();
+CREATE TRIGGER sync_lastupdate BEFORE INSERT OR UPDATE ON author FOR EACH ROW EXECUTE PROCEDURE sync_lastupdate();
