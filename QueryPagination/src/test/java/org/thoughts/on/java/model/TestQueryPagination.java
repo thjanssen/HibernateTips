f51d61bc37cef9d2d0b86674f72cc1583f659acb
Thorben Janssen <thorben@thoughts-on-java.org>
2017-02-04 06:12:26 +0100

    Refactored test case to make it more readable

--- QueryPagination/src/test/java/org/thoughts/on/java/model/TestQueryPagination.java
+++ QueryPagination/src/test/java/org/thoughts/on/java/model/TestQueryPagination.java
@@ -29,31 +29,37 @@ public class TestQueryPagination {
 	}
 
 	@Test
-	public void queryPagination() {
-		log.info("... queryPagination ...");
+	public void first5Authors() {
+		log.info("... first5Authors ...");
 
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
-		
-		List<Author> authors = em.createQuery("SELECT a FROM Author a", Author.class)
-				  .getResultList();
-		Assert.assertEquals("Expected a list of 11 authors.", 11, authors.size());
-		authors.forEach(a -> log.info(a.getFirstName() + " " + a.getLastName()));
-		
-		authors = em.createQuery("SELECT a FROM Author a ORDER BY a.id", Author.class)
-		  .setMaxResults(5)
-		  .setFirstResult(0)
-		  .getResultList();
+
+		List<Author> authors = em.createQuery("SELECT a FROM Author a ORDER BY a.id ASC", Author.class)
+									.setMaxResults(5)
+									.setFirstResult(0)
+									.getResultList();
 		Assert.assertEquals("Expected a list of 5 authors.", 5, authors.size());
 		authors.forEach(a -> log.info(a.getFirstName() + " " + a.getLastName()));
-		
-		authors = em.createQuery("SELECT a FROM Author a ORDER BY a.id", Author.class)
-		  .setMaxResults(5)
-		  .setFirstResult(5)
-		  .getResultList();
+
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	@Test
+	public void next5Authors() {
+		log.info("... next5Authors ...");
+
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		List<Author> authors = em.createQuery("SELECT a FROM Author a ORDER BY a.id ASC", Author.class)
+									.setMaxResults(5)
+									.setFirstResult(5)
+									.getResultList();
 		Assert.assertEquals("Expected a list of 5 authors.", 5, authors.size());
 		authors.forEach(a -> log.info(a.getFirstName() + " " + a.getLastName()));
-		
+
 		em.getTransaction().commit();
 		em.close();
 	}
