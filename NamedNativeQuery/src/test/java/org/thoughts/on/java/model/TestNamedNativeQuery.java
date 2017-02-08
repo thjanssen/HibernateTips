ad9af5d3861e287d3f336ef2af51e8a0023737d5
Thorben Janssen <thorben@thoughts-on-java.org>
2017-02-08 11:07:38 +0100

    Add examples for named and ad-hoc native queries.

---
+++ NamedNativeQuery/src/test/java/org/thoughts/on/java/model/TestNamedNativeQuery.java
@@ -0,0 +1,46 @@
+package org.thoughts.on.java.model;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestNamedNativeQuery {
+
+	Logger log = Logger.getLogger(this.getClass().getName());
+
+	private EntityManagerFactory emf;
+
+	@Before
+	public void init() {
+		emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+	}
+
+	@After
+	public void close() {
+		emf.close();
+	}
+
+	@Test
+	public void namedNativeQuery() {
+		log.info("... namedNativeQuery ...");
+
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		Query q = em.createNamedQuery("selectBook");
+		q.setParameter(0, 1);
+		Book b = (Book) q.getSingleResult();
+		Assert.assertTrue(b instanceof Book);
+		Assert.assertEquals(new Long(1), ((Book)b).getId());
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+}
---
+++ NativeQuery/src/test/java/org/thoughts/on/java/model/TestNativeQuery.java
@@ -0,0 +1,46 @@
+package org.thoughts.on.java.model;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestNativeQuery {
+
+	Logger log = Logger.getLogger(this.getClass().getName());
+
+	private EntityManagerFactory emf;
+
+	@Before
+	public void init() {
+		emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+	}
+
+	@After
+	public void close() {
+		emf.close();
+	}
+
+	@Test
+	public void adHocNativeQuery() {
+		log.info("... adHocNativeQuery ...");
+
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		Query q = em.createNativeQuery("SELECT * FROM book b WHERE id = ?", Book.class);
+		q.setParameter(0, 1);
+		Book b = (Book) q.getSingleResult();
+		Assert.assertTrue(b instanceof Book);
+		Assert.assertEquals(new Long(1), ((Book)b).getId());
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+}
