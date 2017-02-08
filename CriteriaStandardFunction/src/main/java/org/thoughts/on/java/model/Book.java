52ebd93d45c1396047ec4faf45f42815a1325525
Thorben Janssen <thorben@thoughts-on-java.org>
2017-02-08 06:14:36 +0100

    Add example for standard function calls with Criteria API

---
+++ CriteriaStandardFunction/src/main/java/org/thoughts/on/java/model/Book.java
@@ -0,0 +1,126 @@
+package org.thoughts.on.java.model;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Version;
+
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+
+	@Version
+	private int version;
+
+	private String title;
+	
+	private Double price;
+
+	@Temporal(TemporalType.DATE)
+	private Date publishingDate;
+
+	@ManyToOne
+	@JoinColumn(name="publisherid")
+	private Publisher publisher;
+
+	@ManyToMany
+	@JoinTable(
+		      name="BookAuthor",
+		      joinColumns={@JoinColumn(name="bookId", referencedColumnName="id")},
+		      inverseJoinColumns={@JoinColumn(name="authorId", referencedColumnName="id")})
+	private Set<Author> authors = new HashSet<Author>();
+	
+	public Long getId() {
+		return this.id;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getPublishingDate() {
+		return publishingDate;
+	}
+
+	public void setPublishingDate(Date publishingDate) {
+		this.publishingDate = publishingDate;
+	}
+
+	public Publisher getPublisher() {
+		return this.publisher;
+	}
+
+	public void setPublisher(final Publisher publisher) {
+		this.publisher = publisher;
+	}
+
+	public Set<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(Set<Author> authors) {
+		this.authors = authors;
+	}
+	
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Book)) {
+			return false;
+		}
+		Book other = (Book) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		return 31;
+	}
+
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (title != null && !title.trim().isEmpty())
+			result += "title: " + title;
+		return result;
+	}
+}
---
+++ CriteriaStandardFunction/src/test/java/org/thoughts/on/java/model/TestCriteriaConstructor.java
@@ -0,0 +1,58 @@
+package org.thoughts.on.java.model;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Tuple;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCriteriaConstructor {
+
+	Logger log = Logger.getLogger(this.getClass().getName());
+
+	private EntityManagerFactory emf;
+
+	@Before
+	public void init() {
+		emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+	}
+
+	@After
+	public void close() {
+		emf.close();
+	}
+
+	@Test
+	public void callCountFunction() {
+		log.info("... callCountFunction ...");
+
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Tuple> cq = cb.createTupleQuery();
+		Root<Author> root = cq.from(Author.class);
+		cq.multiselect(root, cb.size(root.get(Author_.books)));
+		cq.groupBy(root.get(Author_.id));
+		
+		TypedQuery<Tuple> q = em.createQuery(cq);
+		List<Tuple> results = q.getResultList();
+		
+		for (Tuple r :  results) {
+			log.info(r.get(0) + " wrote " +  r.get(1));
+		}
+
+		em.getTransaction().commit();
+		em.close();
+	}
+}
